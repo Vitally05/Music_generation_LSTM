10e5b1cb5062666c8a495475d1702e344b1527df
eltar14 <lebouant2@gmail.com>
2025-03-20 20:45:35 -0400

    data_preparation.py

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ S'enregistrer avec notre institution au lien suivant : https://huggingface.co/da
 Dans ses paramètres, créer un "Access Tokens" avec permission "Read" (au minimum)
 
 Copier cette clé dans le fichier token_access.py
+
+
+<hr>
+Contraintes actuelles des données en entrée (sinon le modèle a moins de chances de faire une sortie cohérente):  
+
+- Un seul instrument de musique par dataset (ne gère pas les orchestres etc.)
+- Dataset avec un style homogène
---
+++ data_preparation.py
@@ -0,0 +1,94 @@
+from music21 import converter, note, chord
+from pathlib import Path
+import numpy as np
+
+import torch
+import torch.nn.functional as F
+
+
+def get_notes(midi_folder_path, unique=False):
+    """
+    Extract notes and chords from all MIDI files in a given folder.
+    :param midi_folder_path: path of the folder containing the MIDI files. !! all dataset must be from 1 instrument (no duo, etc., same instrument. example : piano.) !!
+    :param  unique: to return a set of the notes (each note appearing at most once)
+            or return all of them (each note appearing at least once, may and will be multiple but necessary to recognize the music patterns).
+    :return: all the notes found in the MIDI files
+    """
+    notes = []
+    midi_folder_path = Path(midi_folder_path).resolve()
+
+    for file in midi_folder_path.glob("*.mid"):
+        midi = converter.parse(str(file))  # music21 expects a str for the path
+        notes_to_parse = midi.flat.notes
+
+        for element in notes_to_parse:
+            duration = element.duration.quarterLength
+            if duration < 0.75:
+                duration_class = 'short'
+            elif duration < 1.5:
+                duration_class = 'medium'
+            else:
+                duration_class = 'long'
+
+            if isinstance(element, note.Note): # if single note
+                note_str = f"{element.pitch}_{duration_class}"
+                notes.append(note_str)
+            elif isinstance(element, chord.Chord): # if chord
+                chord_str = f"{'.'.join(str(n) for n in element.normalOrder)}_{duration_class}"
+                notes.append(chord_str)
+    if unique: # useless mdr
+        return set(notes) #
+    else:
+        return notes
+
+
+def prepare_sequences(notes, n_vocab, sequence_length=100):
+    """
+    Prepare the sequences used by the Neural Network
+    PyTorch version
+
+    :param notes: output from get_notes, ALL notes from the dataset, NOT a set(). Needed to learn the patterns in time
+    :param n_vocab:size of the vocabulary (len(set(notes)) ; output onehot vector size
+    :param sequence_length: May be interisting to change it to see what it changes in the output of the model
+    :return:
+    """
+
+    # get all pitch names
+    pitchnames = sorted(set(item for item in notes))
+
+    # create a dictionary to map pitches to integers
+    note_to_int = {note: number for number, note in enumerate(pitchnames)}
+
+    network_input = []
+    network_output = []
+
+    # create input sequences and the corresponding outputs
+    for i in range(0, len(notes) - sequence_length, 1): # !! important
+        sequence_in = notes[i:i + sequence_length]
+        sequence_out = notes[i + sequence_length]
+        network_input.append([note_to_int[char] for char in sequence_in])
+        network_output.append(note_to_int[sequence_out])
+
+    n_patterns = len(network_input)
+
+    # reshape the input into a format compatible with LSTM layers
+    network_input = np.reshape(network_input, (n_patterns, sequence_length, 1))
+    # normalize input for better stability
+    network_input = network_input / float(n_vocab)
+
+    # convert network_output to a PyTorch tensor and apply one-hot encoding
+    network_output = torch.tensor(network_output, dtype=torch.long)
+    network_output = F.one_hot(network_output, num_classes=n_vocab).float()
+
+    return torch.tensor(network_input, dtype=torch.float32), network_output
+
+
+if __name__ == '__main__':
+    path = "datasets/midi_songs_FF_skuldur" # https://github.com/Skuldur/Classical-Piano-Composer/tree/master/midi_songs
+    notes = get_notes(path)
+    n_vocab = len(set(notes))
+    network_input, network_output = prepare_sequences(notes, n_vocab) # X, y
+
+    print(f"network_input.shape = {network_input.shape} -> network_output.shape = {network_output.shape}")
+    #print(f"{network_input}")
+    #print(f"{network_output}")
